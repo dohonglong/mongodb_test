9d24660cb85a45be998e444b9340468448376746
Long Do <dohonglong1998@gmail.com>
2023-09-28 20:11:37 +0300

    First commit, tryout

---
+++ server/server.js
@@ -0,0 +1,84 @@
+// // To connect with your mongoDB database
+// const mongoose = require("mongoose");
+// mongoose
+//   .connect("mongodb://localhost:27017/", {
+//     dbName: "yourDB-name",
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log("Connected to yourDB-name database"))
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+// // Schema for users of app
+// const UserSchema = new mongoose.Schema({
+//   name: {
+//     type: String,
+//     required: true,
+//   },
+//   email: {
+//     type: String,
+//     required: true,
+//     unique: true,
+//   },
+//   date: {
+//     type: Date,
+//     default: Date.now,
+//   },
+// });
+// const User = mongoose.model("users", UserSchema);
+// User.createIndexes();
+
+// // For backend and express
+// const express = require("express");
+// const app = express();
+// const cors = require("cors");
+// console.log("App listen at port 5000");
+// app.use(express.json());
+// app.use(cors());
+// app.get("/", (req, resp) => {
+//   resp.send("App is Working");
+//   // You can check backend is working or not by
+//   // entering http://loacalhost:5000
+
+//   // If you see App is working means
+//   // backend working properly
+// });
+
+// app.post("/register", async (req, resp) => {
+//   try {
+//     const user = new User(req.body);
+//     let result = await user.save();
+//     result = result.toObject();
+//     if (result) {
+//       delete result.password;
+//       resp.send(req.body);
+//       console.log(result);
+//     } else {
+//       console.log("User already register");
+//     }
+//   } catch (e) {
+//     resp.send("Something Went Wrong");
+//   }
+// });
+// app.listen(5000);
+
+const { MongoClient } = require("mongodb");
+
+// Replace the following with your Atlas connection string
+const url =
+  "mongodb+srv://dohonglong:honglong1998@gettingstarted.crkxfbp.mongodb.net/?retryWrites=true&w=majority";
+// Connect to your Atlas cluster
+const client = new MongoClient(url);
+async function run() {
+  try {
+    await client.connect();
+    console.log("Successfully connected to Atlas");
+  } catch (err) {
+    console.log(err.stack);
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
