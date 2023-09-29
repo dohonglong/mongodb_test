05dccf7a956560b93baaa8e4978a0696261e000e
Long Do <dohonglong1998@gmail.com>
2023-09-29 17:55:06 +0300

    Combine with the register user function

--- server/insert-data.js
+++ server/insert-data.js
@@ -1,43 +1,43 @@
-const { MongoClient } = require("mongodb");
-require("dotenv").config();
+// const { MongoClient } = require("mongodb");
+// require("dotenv").config();
 
-// Replace the following with your Atlas connection string
-const url = `${process.env.MONGO_URI}`;
-const client = new MongoClient(url);
+// // Replace the following with your Atlas connection string
+// const url = `${process.env.MONGO_URI}`;
+// const client = new MongoClient(url);
 
-// Reference the database to use
-const dbName = "gettingStarted";
+// // Reference the database to use
+// const dbName = "gettingStarted";
 
-async function run() {
-  try {
-    // Connect to the Atlas cluster
-    await client.connect();
-    const db = client.db(dbName);
+// async function run() {
+//   try {
+//     // Connect to the Atlas cluster
+//     await client.connect();
+//     const db = client.db(dbName);
 
-    // Reference the "people" collection in the specified database
-    const col = db.collection("people");
+//     // Reference the "people" collection in the specified database
+//     const col = db.collection("people");
 
-    // Create a new document
-    let personDocument = {
-      name: { first: "Long", last: "Do" },
-      birth: new Date(1998, 9, 1), // May 23, 1912
-      // death: new Date(1954, 5, 7), // May 7, 1954
-      contribs: ["Hello", "My name", "is"],
-      views: 1250000,
-    };
+//     // Create a new document
+//     let personDocument = {
+//       name: { first: "Long", last: "Do" },
+//       birth: new Date(1998, 9, 1), // May 23, 1912
+//       // death: new Date(1954, 5, 7), // May 7, 1954
+//       contribs: ["Hello", "My name", "is"],
+//       views: 1250000,
+//     };
 
-    // Insert the document into the specified collection
-    const p = await col.insertOne(personDocument);
+//     // Insert the document into the specified collection
+//     const p = await col.insertOne(personDocument);
 
-    // Find and return the document
-    const filter = { "name.last": "Turing" };
-    const document = await col.findOne(filter);
-    console.log("Document found:\n" + JSON.stringify(document));
-  } catch (err) {
-    console.log(err.stack);
-  } finally {
-    await client.close();
-  }
-}
+//     // Find and return the document
+//     const filter = { "name.last": "Turing" };
+//     const document = await col.findOne(filter);
+//     console.log("Document found:\n" + JSON.stringify(document));
+//   } catch (err) {
+//     console.log(err.stack);
+//   } finally {
+//     await client.close();
+//   }
+// }
 
-run().catch(console.dir);
+// run().catch(console.dir);
--- server/server.js
+++ server/server.js
@@ -1,71 +1,20 @@
-// // To connect with your mongoDB database
-// const mongoose = require("mongoose");
-// mongoose
-//   .connect("mongodb://localhost:27017/", {
-//     dbName: "yourDB-name",
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => console.log("Connected to yourDB-name database"))
-//   .catch((error) => {
-//     console.log(error);
-//   });
-
-// // Schema for users of app
-// const UserSchema = new mongoose.Schema({
-//   name: {
-//     type: String,
-//     required: true,
-//   },
-//   email: {
-//     type: String,
-//     required: true,
-//     unique: true,
-//   },
-//   date: {
-//     type: Date,
-//     default: Date.now,
-//   },
-// });
-// const User = mongoose.model("users", UserSchema);
-// User.createIndexes();
-
-// // For backend and express
-// const express = require("express");
-// const app = express();
-// const cors = require("cors");
-// console.log("App listen at port 5000");
-// app.use(express.json());
-// app.use(cors());
-// app.get("/", (req, resp) => {
-//   resp.send("App is Working");
-//   // You can check backend is working or not by
-//   // entering http://loacalhost:5000
-
-//   // If you see App is working means
-//   // backend working properly
-// });
-
-// app.post("/register", async (req, resp) => {
-//   try {
-//     const user = new User(req.body);
-//     let result = await user.save();
-//     result = result.toObject();
-//     if (result) {
-//       delete result.password;
-//       resp.send(req.body);
-//       console.log(result);
-//     } else {
-//       console.log("User already register");
-//     }
-//   } catch (e) {
-//     resp.send("Something Went Wrong");
-//   }
-// });
-// app.listen(5000);
-
+// For backend and express
 const { MongoClient } = require("mongodb");
 require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+//console.log("App listen at port 5000");
+app.use(express.json());
+app.use(cors());
+app.get("/", (req, resp) => {
+  resp.send("App is Working");
+  // You can check backend is working or not by
+  // entering http://loacalhost:5000
+
+  // If you see App is working means
+  // backend working properly
+});
 
 // Replace the following with your Atlas connection string
 const url = `${process.env.MONGO_URI}`;
@@ -83,3 +32,55 @@ async function run() {
   }
 }
 run().catch(console.dir);
+
+// To connect with your mongoDB database
+const mongoose = require("mongoose");
+mongoose
+  .connect(`${process.env.MONGO_URI}`, {
+    dbName: "myDB_Profile",
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to myDB_Profile"))
+  .catch((error) => {
+    console.log(error);
+  });
+
+// Schema for users of app
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    //unique: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+const User = mongoose.model("users", UserSchema);
+User.createIndexes();
+
+// Register input to MongoDB database
+app.post("/register", async (req, resp) => {
+  try {
+    const user = new User(req.body);
+    let result = await user.save();
+    result = result.toObject();
+    if (result) {
+      delete result.password;
+      resp.send(req.body);
+      console.log(result);
+    } else {
+      console.log("User already register");
+    }
+  } catch (e) {
+    console.log(e);
+    resp.send("Something Went Wrong");
+  }
+});
+app.listen(5000);
